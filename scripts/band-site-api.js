312b4849a9ebeed9d2fd720070c43372c33e0d0a
unknown <lpreveton@gmail.com>
2024-02-15 12:59:40 -0800

    initial method of Band-Site class

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -2,20 +2,42 @@ import axios from "axios";
 
 const key = "eab30806-8fdc-432d-8db2-e067e49bb38c";
 
-class BandSiteApi {
+export class BandSiteApi {
   constructor(apiKey) {
     (this.apiKey = apiKey),
-      (this.baseUrl =
-        "https://unit-2-project-api-25c1595833b2.herokuapp.com/comments");
+      (this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com");
   }
-  async postComment(comment) {}
-  async getComment() {
-    const response = await axios.get(this.baseUrl, {
+
+  async getComments() {
+    const response = await axios.get(this.baseUrl + "/comments", {
       params: {
-        api_key: "eab30806-8fdc-432d-8db2-e067e49bb38c",
+        api_key: this.apiKey,
       },
     });
-    console.log(response.data);
+    console.log("Get comments: ", response);
   }
-  async getShow() {}
+  async postComment(comment) {
+    const response = await axios.post(this.baseUrl + "/comments", {
+      params: {
+        api_key: this.apiKey,
+      },
+      header: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        name: comment.name,
+        comment: comment.comment,
+      },
+    });
+    console.log("Post", response);
+  }
+
+  async getShow() {
+    const response = await axios.get(this.baseUrl + "/showdates", {
+      params: {
+        api_key: this.apiKey,
+      },
+    });
+    console.log("Get", response);
+  }
 }
